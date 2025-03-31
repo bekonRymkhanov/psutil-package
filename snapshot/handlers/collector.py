29c7482cdc95265647d511623d4138ca18f71d42
bekonRymkhanov <bekarys2004r@gmail.com>
2025-03-31 18:23:19 +0500

    sef

--- snapshot/handlers/collector.py
+++ snapshot/handlers/collector.py
@@ -13,8 +13,8 @@ class SnapshotCollector:
         return {
             "Tasks": self.get_task_summary(),
             "%CPU": {"user": cpu_times.user, "system": cpu_times.system, "idle": cpu_times.idle},
-            "KiB Mem": {"total": mem.total//100, "free": mem.free//100, "used": mem.used//100},
-            "KiB Swap": {"total": swap.total//100, "free": swap.free//100, "used": swap.used//100},
+            "KiB Mem": {"total": mem.total//1000, "free": mem.free//1000, "used": mem.used//1000},
+            "KiB Swap": {"total": swap.total//1000, "free": swap.free//1000, "used": swap.used//1000},
             "Timestamp": int(time.time())
         }
 
